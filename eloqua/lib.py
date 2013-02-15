2bc714c774be3b975328be98c2b8f3b37a5ac421
Sander van de Graaf <mail@svdgraaf.nl>
2013-02-15 16:41:30 +0100

    move regular get/deletes to base class, added Searchclass

--- eloqua/lib.py
+++ eloqua/lib.py
@@ -13,12 +13,15 @@ class EloquaBaseClient(object):
         authKey = base64.b64encode(key)
         self.headers = {"Content-Type":"application/json", "Authorization":"Basic " + authKey}
 
+    def get(self, identifier):
+        """Return the asset by the given id"""
+        return self._get_by_id(identifier)
+
     def _get_by_id(self, identifier):
         # primary url base seems out of whack
         url = self.base_url + '/' + str(identifier)
         response = requests.get(url, headers=self.headers)
         return response.json()
-
 
     # Delete asset by id, return True or False when succesful
     def _delete_by_id(self, identifier):
@@ -29,7 +32,12 @@ class EloquaBaseClient(object):
         else:
             return False
 
-class EloquaClient(object):
+    def delete(self, identifier):
+        """Deletes the asset by the given id"""
+        return self._delete_by_id(identifier)
+
+
+class EloquaClient(EloquaBaseClient):
     headers = ''
     base_url = settings.BASE_URL
 
@@ -37,16 +45,17 @@ class EloquaClient(object):
     def emails(self):
         return EloquaEmailClient()
 
+    @property
+    def contacts(self):
+        return EloquaContactsClient()
+
 
 class EloquaEmailClient(EloquaBaseClient):
     # only get/search need the slash
     base_url = settings.BASE_URL + '/assets/email'
 
-    def get(self, identifier):
-        return self._get_by_id(identifier)
-
     def create(self, name, subject, body, body_plaintext=None, is_tracked=False, folder_id=None, sender_name=None, sender_email=None, reply_to_email=None, reply_to_name=None):
-
+        """Creates an email with the given parameters, returns a json representation"""
         payload = {
             'htmlContent': {
                 'html': body,
@@ -82,6 +91,32 @@ class EloquaEmailClient(EloquaBaseClient):
         else:
             raise Exception('error!')  # TODO: be more verbose
 
-    def delete(self, identifier):
-        return self._delete_by_id(identifier)
+
+class EloquaContactsClient(EloquaBaseClient):
+    # only get/search need the slash
+    base_url = settings.BASE_URL + '/data/contact'
+
+    def search(self, query, page=1, count=100, depth='complete'):
+        """Search all contacts for given email query, eg:
+        
+            from eloqua.lib import *
+            e = EloquaClient()
+            e.contacts.search('foobar@example.com')
+                {u'elements': [{u'createdAt': u'1327661283',
+                u'currentStatus': u'Awaiting action',
+                u'depth': u'complete',
+                u'emailAddress': u'foobar@example.com',
+                ...
+
+        """
+        url = self.base_url + 's'
+        payload = {
+            'search': query,
+            'page': page,
+            'count': count,
+            'depth': depth,
+        }
+
+        r = requests.get(url, params=payload, headers=self.headers)
+        return r.json()
 
